a091a2685c29c5e5db4c6fe45ebf8af6ab58f2d1
MariaMartin80 <77515936+MariaMartin80@users.noreply.github.com>
2021-02-05 13:00:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Please enter your name: ");
+            String name = Console.ReadLine();
+               PrintName(name);                                                                     
+
+        }
+        static void PrintName(String name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+            Console.ReadLine();
+                
+        }
+        
+    }
+}
